9de1f5149046c6e8524f93ebe039b137e9a3d970
bdurand2 <benjamin.durand.dev@protonmail.com>
2020-06-12 16:07:14 +0200

    Misc/ Update project name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Benjamin Durand`,
+    description: `Full Stack Developer`,
+    author: `@bdurand2`,
     social: {
       linkedin: `https://www.linkedin.com/in/benjamin-durand-3aa0a61a4/`,
       github: `https://github.com/bdurand2`
